24e64ea09efd215a7ecea92d079cc5267871b1aa
MaksimKrishtopa <k.maks21@mail.ru>
2024-02-13 22:20:47 +0700

    Вычисляемые свойства, практическая 7

--- js/main.js
+++ js/main.js
@@ -2,23 +2,27 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
+        onSale: true,
         altText: "A pair of socks",
-        inStock: false,
         inventory: 100,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'Green',
+                variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
-                variantColor: 'Blue',
+                variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
-        ],
+         ],
+         
         
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0, 
@@ -35,13 +39,36 @@ let app = new Vue({
             this.inventory += 1
         },
 
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
         },
 
 
+     },
+
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+
+        sale() {
+            if (this.onSale) {
+                return this.title + ' is on sale!';
+            } else {
+                return this.title + ' is not on sale.';
+            }
+        }
          
-     }
+ 
+     },
      
  })
  
